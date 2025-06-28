70aecb3dd2a89aaa682638a22fd24c058b30970d
Maximilian <mpinderwhite@gmail.com>
2025-06-28 19:11:26 +0200

    added fonts

--- modules/home-manager/fonts.nix
+++ modules/home-manager/fonts.nix
@@ -7,15 +7,28 @@
     nerd-fonts.jetbrains-mono
     nerd-fonts.hack
     nerd-fonts.iosevka
+    nerd-fonts.cascadia-code
+    nerd-fonts.ubuntu-mono
+    nerd-fonts.terminus
 
     # Additional useful fonts
     fira-code
     jetbrains-mono
     source-code-pro
+    ibm-plex
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    roboto
+    roboto-mono
+    ubuntu_font_family
+    cantarell-fonts
 
     # System fonts
     liberation_ttf
     dejavu_fonts
+    unifont
+    dina-font
   ];
 
   fonts.fontconfig = {
