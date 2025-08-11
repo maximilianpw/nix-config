eaaafab6e8e34caa7c2c322e2f6ca5dd88be542b
Maximilian PINDER-WHITE <mpinderwhite@proton.me>
2025-08-11 21:17:50 +0200

    hypr

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -14,11 +14,6 @@
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-hyprland pkgs.xdg-desktop-portal-gtk ];
 
-  # Keep env vars lean; add only if necessary later
-  environment.sessionVariables = {
-    MOZ_ENABLE_WAYLAND = "1";
-  };
-
   # Minimal supporting tools
   environment.systemPackages = [
     pkgs.wl-clipboard
@@ -26,13 +21,4 @@
     pkgs.kdePackages.dolphin
     pkgs.wofi
   ];
-
-  services.seatd.enable = true;             # provide a seat when no “big” DM is used
-  services.greetd = {
-    enable = true;
-    package = pkgs.greetd.tuigreet;
-    settings.default_session = {
-      command = "tuigreet --time --remember --cmd 'dbus-run-session Hyprland'";
-      user = "maxpw";
-    };
-  };}
+}
