e8b18e74a2a7908e8b7a1ab258360b1de618f41b
Maximilian PINDER-WHITE <mpinderwhite@proton.me>
2025-10-14 17:31:17 +0200

    NixOS/Darwin rebuild: main-pc - Generation 2025-10-14 17:31:15 (current)

--- packages/helium.nix
+++ packages/helium.nix
@@ -2,25 +2,42 @@
   pkgs,
   lib,
   fetchurl,
-}:
-pkgs.appimageTools.wrapType2 {
+}: let
   pname = "helium";
   version = "0.5.5.2";
 
   src = fetchurl {
-    url = "https://github.com/imputnet/helium-linux/releases/download/0.5.5.2/helium-0.5.5.2-x86_64.AppImage";
+    url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-x86_64.AppImage";
     sha256 = "d9ffef79779432bfdc861d184812c3f3ada7bf946d272e6a95a03b7e1bab5a80";
   };
 
-  extraPkgs = pkgs:
-    with pkgs; [
-      # Add any additional dependencies here if needed
-    ];
-
-  meta = with lib; {
-    description = "Helium Browser";
-    homepage = "https://github.com/imputnet/helium-linux";
-    license = licenses.mit;
-    platforms = ["x86_64-linux"];
+  appimageContents = pkgs.appimageTools.extractType2 {
+    inherit pname version src;
   };
-}
+in
+  pkgs.appimageTools.wrapType2 {
+    inherit pname version src;
+
+    extraInstallCommands = ''
+      # Install desktop file with correct Exec path
+      install -m 444 -D ${appimageContents}/helium.desktop $out/share/applications/helium.desktop
+      substituteInPlace $out/share/applications/helium.desktop \
+        --replace 'Exec=AppRun' "Exec=${pname}" \
+        --replace 'Icon=helium' "Icon=$out/share/pixmaps/helium.png"
+
+      # Install icon
+      install -m 444 -D ${appimageContents}/helium.png $out/share/pixmaps/helium.png
+    '';
+
+    extraPkgs = pkgs:
+      with pkgs; [
+        # Add any additional dependencies here if needed
+      ];
+
+    meta = with lib; {
+      description = "Helium Browser";
+      homepage = "https://github.com/imputnet/helium-linux";
+      license = licenses.mit;
+      platforms = ["x86_64-linux"];
+    };
+  }
