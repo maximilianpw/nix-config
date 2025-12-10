619fb57110aad467b476c1b0f8e4d9cf48979cac
Maximilian PINDER-WHITE <mpinderwhite@proton.me>
2025-12-10 08:21:57 +0100

    update packages

--- modules/core/nix-settings.nix
+++ modules/core/nix-settings.nix
@@ -3,6 +3,7 @@
     experimental-features = ["nix-command" "flakes"];
     keep-outputs = true;
     keep-derivations = true;
+    nix.settings.trusted-users = ["root" "max-vev"];
     trusted-users = ["root" "maxpw"];
   };
 }
