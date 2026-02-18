44448f58f6d59e38621a8c744ef8b10ad64a1f3f
Maximilian PINDER-WHITE <mpinderwhite@proton.me>
2026-02-18 21:17:23 +0100

    NixOS/Darwin rebuild: main-pc - Generation 2026-02-18 21:17:22 (current)

--- users/maxpw/modules/packages/linux-desktop.nix
+++ users/maxpw/modules/packages/linux-desktop.nix
@@ -7,41 +7,41 @@
 }: {
   home.packages =
     lib.optionals isLinux [
-    # App launcher
-    pkgs.rofi
+      # App launcher
+      pkgs.rofi
 
-    # Terminal emulator
-    pkgs.ghostty
+      # Terminal emulator
+      pkgs.ghostty
 
-    # Wayland desktop essentials
-    pkgs.waybar
-    pkgs.wl-clipboard
-    pkgs.cliphist
-    pkgs.grim
-    pkgs.slurp
-    pkgs.hyprpaper
-    pkgs.hypridle
-    pkgs.swaynotificationcenter
-    pkgs.gammastep
-    pkgs.wlogout
-    pkgs.networkmanagerapplet
+      # Wayland desktop essentials
+      pkgs.waybar
+      pkgs.wl-clipboard
+      pkgs.cliphist
+      pkgs.grim
+      pkgs.slurp
+      pkgs.hyprpaper
+      pkgs.hypridle
+      pkgs.swaynotificationcenter
+      pkgs.gammastep
+      pkgs.wlogout
+      pkgs.networkmanagerapplet
 
-    # GUI applications
-    pkgs.nautilus
-    pkgs.discord
-    pkgs.mongodb-compass
-    pkgs.protonmail-desktop
-    pkgs.mullvad-vpn
-    pkgs.obsidian
-    pkgs._1password-gui
+      # GUI applications
+      pkgs.nautilus
+      pkgs.discord
+      pkgs.mongodb-compass
+      pkgs.protonmail-desktop
+      pkgs.mullvad-vpn
+      pkgs.obsidian
+      pkgs._1password-gui
 
-    # System utilities
-    pkgs.pavucontrol
-    pkgs.brightnessctl
-    pkgs.playerctl
-    pkgs.qbittorrent
-    pkgs.cava
-  ]
+      # System utilities
+      pkgs.pavucontrol
+      pkgs.brightnessctl
+      pkgs.playerctl
+      pkgs.qbittorrent
+      pkgs.cava
+    ]
     ++ lib.optionals (isLinux && hostname != "main-pc") [
       pkgs.hyprlock
     ];
