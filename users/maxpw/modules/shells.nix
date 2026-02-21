8f3666027c101399e65f74ebca3cb11006d75f47
Maximilian PINDER-WHITE <mpinderwhite@proton.me>
2026-02-21 21:36:53 +0100

    add tmux aliases

--- users/maxpw/modules/shells.nix
+++ users/maxpw/modules/shells.nix
@@ -38,7 +38,6 @@
     gst = "git status";
     gco = "git checkout";
     gcob = "git checkout -b";
-    gcp = "git cherry-pick";
     gd = "git diff";
     gl = "git prettylog";
     gp = "git push";
@@ -62,13 +61,14 @@
     chcd = "chezmoi cd";
     chap = "chezmoi apply";
 
+    tm = "tmux";
+    tma = "tmux attach";
+    tml = "tmux list-sessions";
+    tmka = "tmux kill-sessions -a";
+
     # Nix system management aliases
     nr = "make -C ~/nix-config rebuild";
     nup = "make -C ~/nix-config update";
-    nch = "make -C ~/nix-config check";
-    ngc = "make -C ~/nix-config gc";
-    ngen = "make -C ~/nix-config generations";
-    ninfo = "make -C ~/nix-config info";
 
     # Shortcut to setup a nix-shell with fish. This lets you do something like
     # `fnix -p go` to get an environment with Go but use the fish shell along
