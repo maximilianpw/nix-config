1cb432142e4a2dc09d83d6c21fafe4270e8834a1
Maximilian PINDER-WHITE <mpinderwhite@proton.me>
2026-04-06 17:49:54 +0200

    update helium

--- packages/helium.nix
+++ packages/helium.nix
@@ -5,11 +5,11 @@
   fetchurl,
 }: let
   pname = "helium";
-  version = "0.9.2.1";
+  version = "0.10.8.1";
 
   src = fetchurl {
     url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-x86_64.AppImage";
-    sha256 = "82e0c122bf0d383d06b635b39ec55796f6fa965bdd587c5111f0ef5de3f89bfc";
+    sha256 = "a4dfc8c3500d8200cec7115bd82378dfaa9b72bb3cfdb0dc12a8d90bcd20ad0b";
   };
 
   appimageContents = pkgs.appimageTools.extractType2 {
