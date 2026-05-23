4c83723841c2f75e736a7ab0922732b26c33af74
Maximilian PINDER-WHITE <mpinderwhite@proton.me>
2026-05-23 22:42:16 +0200

    add numtide cache

--- modules/core/nix-settings.nix
+++ modules/core/nix-settings.nix
@@ -16,11 +16,13 @@
       "https://cache.nixos.org"
       "https://hyprland.cachix.org"
       "https://maximilianpw.cachix.org"
+      "https://numtide.cachix.org"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "maximilianpw.cachix.org-1:RgUBJCLYTHNEeg67Pht2cf6VGG2NQnyxmn6jTCU+TsA="
+      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
     ];
   };
 
